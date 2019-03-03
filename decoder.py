e03e7a99e163c7e17a2c11e2c33a9f01b470ba9a
Alexandr Tarakanov <sundoz@mail.ru>
2019-03-04 01:33:21 +0300

    zadacha pro codi decodi

---
+++ decoder.py
@@ -0,0 +1,34 @@
+a2=str(input())
+a=[]
+for i in a2:
+    a+=i
+b2= input()
+b=[]
+for i in b2:
+    b+=i
+d={}
+d1={}
+for i in range(len(a)):
+    d[a[i]]=b[i]
+    d1[b[i]]=a[i]
+c=input()
+c2=[]
+for i in c:
+    c2+=i
+
+c1=[]
+for i in c2:
+    c1+=d[i]
+f=input()
+f1=[]
+f2=[]
+for i in f:
+    f1+=i
+for i in f1:
+    f2+=d1[i]
+for i in range(len(c1)):
+    print(c1[i],end='')
+for j in range(len(f2)):
+    print(f2[j],end='')
+
+

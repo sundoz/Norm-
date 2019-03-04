4a760c04d35c04e3b93347fd9eac700e886a0eca
Alexandr Tarakanov <sundoz@mail.ru>
2019-03-04 23:37:25 +0300

    proverkaorfografii

--- proverkaorfografii.py
+++ proverkaorfografii.py
@@ -1,15 +1,13 @@
-n=int(input())
-a=[]
 d={}
+d1={}
+n=int(input())
 for i in range(n):
     d[input().lower()]=''
 m=int(input())
-
 for i in range(m):
-    a[i]=input().split()
-for i in range(m):
-    for j in a[i].lower():
-        if j not in d :
-            print(j)
-            d[j]=''
-        
+    a=input().split()
+    for j in a:
+        if str(j).lower() not in d:
+            d1[j]=''
+for i in d1.keys():
+    print

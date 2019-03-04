5a5450b0e2e386dbfedf8c50903092daf9d0baca
Alexandr Tarakanov <sundoz@mail.ru>
2019-03-05 01:00:08 +0300

    -kursnastepike

--- Untitled-8.py
+++ Untitled-8.py
@@ -2,15 +2,15 @@ a=[]
 d={}
 while a!='':
     a=input().split()
-    if a=="":
+    if a==[]:
         break
     if a[0] not in d:
         d[a[0]]=[0,0]
     d[a[0]][0]+=int(a[2])
     d[a[0]][1]+=1
 for i in range(11):
-    if str(i) not in d :
-        print(i,' ','-')
+    if str(i+1) not in d :
+        print(i+1,' ','-')
     else:
-         print(i,' ',d[str(i)])
+         print(i+1,' ',d[str(i+1)][0]/d[str(i+1)][1])
 

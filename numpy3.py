b49fea619850540028de4621cd08a0a9f9b204e1
Alexandr Tarakanov <sundoz@mail.ru>
2019-03-10 20:38:41 +0300

    book

---
+++ numpy3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+deg f(x,coeff):
+
+    X=np.empty(len(coeff))
+    x[0]=1
+    x[1:]=x
--- str.py
+++ str.py
@@ -1,7 +1,8 @@
 a=str(input())
 count=0
 for i in range(len(a)):
-    if a[i]==a[i].upper()
+    if a[i]==a[i].upper():
+        
     count+=1
 print(count)
 
